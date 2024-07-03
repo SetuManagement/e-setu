a696b1486a60c429ef3db48cf7ccdf237f4b8d2a
AKSHAY\akshay <akshay.landage@rhealtech.com>
2024-07-03 23:37:01 +0530

    initial setup

---
+++ ESetu.UI/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PrimeNGConfig } from 'primeng/api';
+import { PrimeNgModule } from '../modules/primeng.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PrimeNgModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
